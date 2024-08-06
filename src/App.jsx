b316ed50b8ad2bb7134409e707b24e83110e4cc1
Mattia Lavai <mattialavai@gmail.com>
2024-08-06 00:16:54 +0000

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
-import Chat from "./components/chat/Chat";
-import Detail from "./components/detail/Detail";
-import List from "./components/list/List";
-import Login from "./components/login/Login";
-import Notification from "./components/notification/Notification";
+import { useEffect, Suspense, lazy } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 import { useUserStore } from "./lib/userStore";
 import { useChatStore } from "./lib/chatStore";
+
+// Use lazy for dynamic import
+const Chat = lazy(() => import("./components/chat/Chat"));
+const Detail = lazy(() => import("./components/detail/Detail"));
+const List = lazy(() => import("./components/list/List"));
+const Login = lazy(() => import("./components/login/Login"));
+const Notification = lazy(() => import("./components/notification/Notification"));
 
 const App = () => {
   const { currentUser, isLoading, fetchUserInfo } = useUserStore();
@@ -27,16 +29,18 @@ const App = () => {
 
   return (
     <div className="container">
-      {currentUser ? (
-        <>
-          <List />
-          {chatId && <Chat />}
-          {chatId && <Detail />}
-        </>
-      ) : (
-        <Login />
-      )}
-      <Notification />
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        {currentUser ? (
+          <>
+            <List />
+            {chatId && <Chat />}
+            {chatId && <Detail />}
+          </>
+        ) : (
+          <Login />
+        )}
+        <Notification />
+      </Suspense>
     </div>
   );
 };
---
+++ src/components/HeavyComponent.jsx
@@ -0,0 +1,7 @@
+// src/components/HeavyComponent.js
+
+const HeavyComponent = () => {
+  return <div>This is a heavy component</div>;
+};
+
+export default HeavyComponent;
